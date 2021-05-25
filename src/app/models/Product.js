8f6e19382e9c7c8ba3246bdcb2fdce11fb9c4aac
Luã Mourão <luan.gcc@gmail.com>
2021-05-25 18:41:31 -0300

    /front-end/ criação e estilização da landing page (home/index.njk), que traz os 3 produtos mais recentes; /back-end/ criação da rota da landing page e do respectivo Controller (HomeController.js) trazendo o método 'index' responsável por organizar os dados dos produtos e enviar para a landing os 3 primeiros; criação do método 'all' no Model de produto (Product.js) para buscar todos os produtos cadastrados no banco

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -2,6 +2,14 @@ const db = require('../../config/db');
 
 module.exports = {
 
+  all() {
+
+    return db.query(`
+      SELECT * FROM products
+      ORDER BY updated_at DESC
+    `);
+  },
+
   create(data) {
     const query = `
       INSERT INTO products (
