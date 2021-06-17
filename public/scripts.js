c68ee573f2a30a920802002e60b30c9c6a9ddaf7
Luã Mourão <luan.gcc@gmail.com>
2021-06-16 22:48:02 -0300

    /front-end/ criação do objeto Validate com os métodos de validação do email, CEP, CPF e CNPJ; criação e estilização do alerta de dado incorreto no corpo do formulário e da mensagem de alerta exibida no topo da tela caso dados inválidos sejam submetidos (validação back-end); refatoração das views, separando os links de estilos CSS num arquivo njk distinto (styles.njk)

--- public/scripts.js
+++ public/scripts.js
@@ -214,5 +214,81 @@ const Lightbox = {
     target.style.bottom = "initial";
     closeButton.style.top = "-80px";
   }
-}
+};
+
+const Validate = {
+
+  apply(input, method) {
+    this.clearErrors(input);
+
+    let results = Validate[method](input.value);
+    input.value = results.value;
+
+    if (results.error) {
+      this.displayError(input, results.error);
+    }
+
+  },
+  
+  displayError(input, error) {
+    const div = document.createElement('div');
+    div.classList.add('error');
+    div.innerHTML = error;
+    input.parentNode.appendChild(div);
+    input.focus();
+  },
+
+  clearErrors(input) {
+    const errorDiv = input.parentNode.querySelector('.error');
+
+    if (errorDiv) {
+      errorDiv.remove();
+    }
+  },
+
+  isEmail(value) {
+    let error = null;
+    const mailFormat = /\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+
+    if (!value.match(mailFormat)) {
+      error = "Email incorreto";
+    }
+
+    return {
+      error,
+      value
+    }
+  },
+
+  isCpfCnpj(value) {
+    let error = null;
+    const cleanValues = value.replace(/\D/g, "");
+
+    if (cleanValues.length > 11 && cleanValues.length !== 14) {
+      error = "CNPJ inválido";
+    } else if (cleanValues.length < 12 && cleanValues.length !==11) {
+        error = "CPF inválido";
+    }
+
+    return {
+      error,
+      value
+    }
+  },
+
+  isCep(value) {
+    let error = null;
+    const cleanValues = value.replace(/\D/g, "");
+
+    if (cleanValues.length !== 8) {
+      error = "CEP inválido";
+    }
+
+    return {
+      error,
+      value
+    }
+  }
+
+};
 
