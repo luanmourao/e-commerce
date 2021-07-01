bfebf00379846fe6e6078a4a11875404451e7f76
Luã Mourão <luan.gcc@gmail.com>
2021-07-01 01:03:17 -0300

    Altera as rotas de busca e exibição de produtos

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,11 +6,11 @@ const SearchController = require('../app/controllers/SearchController');
 const { onlyUsers } = require('../app/middlewares/session');
 
 routes.get('/create', onlyUsers, ProductController.create);
-routes.get('/:id', ProductController.show);
+routes.get('/product/:id', ProductController.show);
+routes.get('/product/:id/edit', onlyUsers, ProductController.edit);
+routes.get('/search', SearchController.index);
 routes.post('/', onlyUsers, multer.array("photos", 6), ProductController.post);
-routes.get('/:id/edit', onlyUsers, ProductController.edit);
 routes.put('/', onlyUsers, multer.array("photos", 6), ProductController.put);
 routes.delete('/', onlyUsers, ProductController.delete);
-routes.get('/search', SearchController.index);
 
 module.exports = routes;
