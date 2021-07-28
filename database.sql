7e3d46d527afc8bc3da6f8336c41d256d4573472
Luã Mourão <luan.gcc@gmail.com>
2021-07-28 12:24:40 -0300

    Implementa estratégia soft delete na tabela de produtos e cria método de busca
    A estratégia foi adotada para garantir a integridade dos dados da tabela 'orders' (pedidos), que deve se manter funcional para todos os seus registros ainda que um produto, ao qual o registro se refere, tenha sido excluído pelo seu vendedor. Para isto, foi criada a coluna 'deleted_at' na tabela 'products' que terá seu valor alterado por uma RULE sempre que uma operação de deleção for chamada para um registro. Em seguida, foi criada uma VIEW para representar a tabela 'products' em seus registros sem valor truthy para o atributo 'deleted_at'. Por fim, foram renomeadas tanto a VIEW quanto a tabela de produtos para que o padrão seja a interação com a VIEW e apenas ações específicas possam interagir com a tabela de produtos.

--- database.sql
+++ database.sql
@@ -135,3 +135,22 @@ CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON orders
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- SOFT DELETION
+-- 1. Cria a coluna "deleted_at" na tabela "products"
+ALTER TABLE products ADD COLUMN "deleted_at" timestamp;
+
+-- 2. Cria regra que vai ser aplicada quando a deleção de um registro na tabela de produtos for solicitada
+CREATE OR REPLACE RULE delete_product AS
+ON DELETE TO products DO INSTEAD
+UPDATE products
+SET deleted_at = now()
+WHERE products.id = old.id;
+
+-- 3. Cria VIEW para representar apenas os registros ativos da tabela "products", ou seja, os registros com valor falsy para o atributo "deleted_at"
+CREATE VIEW active_products AS
+SELECT * FROM products WHERE deleted_at IS null;
+
+-- 4. Renomeia a VIEW e a table representada
+ALTER TABLE products RENAME TO products_with_deleted;
+ALTER VIEW active_products RENAME TO products;
