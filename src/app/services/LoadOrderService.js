4e315a65ea04f49c52cdc6214de651a591e044b7
Luã Mourão <luan.gcc@gmail.com>
2021-07-30 10:14:06 -0300

    Cria um service para carregar e formatar os pedidos

---
+++ src/app/services/LoadOrderService.js
@@ -0,0 +1,62 @@
+const { formatPrice, date } = require('../../lib/utils');
+const Order = require('../models/Order');
+const User = require('../models/User');
+const LoadProductService = require('./LoadProductService');
+
+async function format(order) {
+  order.product = await LoadProductService.load('productWithDeleted', { where: { id: order.product_id } });
+  order.buyer = await User.findOne({ where: { id: order.buyer_id } });
+  order.seller = await User.findOne({ where: { id: order.seller_id } });
+
+  order.formattedPrice = formatPrice(order.price);
+  order.formattedTotal = formatPrice(order.total);
+
+  const status = {
+    open: 'Aberto',
+    sold: 'Vendido',
+    canceled: 'Cancelado'
+  };
+  order.formattedStatus = status[order.status];
+
+  const updatedAt = date(order.updated_at);
+  order.formattedUpdatedAt = `${order.formattedStatus} em ${updatedAt.format} às ${updatedAt.hour}h${updatedAt.minutes}`;
+
+  return order;
+}
+
+const LoadService = {
+
+  load(service, filter) {
+    this.filter = filter;
+
+    return this[service]();
+  },
+
+  async order(){
+    try {
+      const order = await Order.findOne(this.filter);
+      return format(order);
+      
+    } catch (error) {
+        console.error(error);
+    }
+  },
+
+  async orders(){
+    try {
+      const orders = await Order.findAll(this.filter);
+
+      const ordersPromise = orders.map(format);
+
+      return Promise.all(ordersPromise);
+      
+    } catch (error) {
+        console.error(error);
+    }
+  },
+
+  format
+
+}
+
+module.exports = LoadService;
